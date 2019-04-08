8627f2ee22c9fd0996a7f134dd26467450201c6a
Raphael Tremblay <raphaeltremblay01@gmail.com>
2019-04-08 00:12:04 -0400

    Make working shared lib CGE with mingw

--- src/States/PlayingState.cpp
+++ src/States/PlayingState.cpp
@@ -13,6 +13,8 @@
 #include <Utils/TimeUtils.h>
 #include <Text/TextRenderer.h>
 
+static const float INFO_X_POSITION = 0.65f;
+
 PlayingState::PlayingState()
         : grid_(1, -0.975f, 0.25f, -0.975f, 10)
 {
@@ -35,10 +37,10 @@ PlayingState::PlayingState()
             std::bind(&PlayingState::keyCallBack, this, std::placeholders::_1, std::placeholders::_2,
                       std::placeholders::_3));
 
-    CGE::GUI::GUIManager::addComponent(new CGE::GUI::Panel({0.735f, 0.73f}, {0.25f, 0.25f}, 20001,
+    CGE::GUI::GUIManager::addComponent(new CGE::GUI::Panel({INFO_X_POSITION, 0.73f}, {0.25f, 0.25f}, 20001,
                                                            [](int key, int action){}, true));
 
-    grid_.setFuturPieceLocation({0.735f, 0.73f, 0.8f});
+    grid_.setFuturPieceLocation({INFO_X_POSITION, 0.73f, 0.8f});
 }
 
 PlayingState::~PlayingState()
@@ -65,21 +67,21 @@ void PlayingState::draw()
     //Render game info
     auto gameInfo = grid_.getInfo();
     //TIME
-    CGE::Text::textRenderer::renderText("TIME", 0.73, 0.3f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText("TIME", INFO_X_POSITION, 0.3f, 0.4f, {0, 0, 0});
     std::string time = CGE::Utils::timeToString(CGE::Utils::getChronoTime(0), 4, false);
-    CGE::Text::textRenderer::renderText(time.substr(0, 5), 0.73, 0.15f, 0.4f, {0, 0, 0});
-    CGE::Text::textRenderer::renderText(time.substr(5, time.size() - 1), 0.73, 0.0f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText(time.substr(0, 5), INFO_X_POSITION, 0.15f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText(time.substr(5, time.size() - 1), INFO_X_POSITION, 0.0f, 0.4f, {0, 0, 0});
     //SCORE
-    CGE::Text::textRenderer::renderText("SCORE", 0.73, -0.2f, 0.4f, {0, 0, 0});
-    CGE::Text::textRenderer::renderText(std::to_string(std::get<1>(gameInfo)), 0.73, -0.35f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText("SCORE", INFO_X_POSITION, -0.2f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText(std::to_string(std::get<1>(gameInfo)), INFO_X_POSITION, -0.35f, 0.4f, {0, 0, 0});
 
     //Level
-    CGE::Text::textRenderer::renderText("LEVEL", 0.73, -0.5f, 0.4f, {0, 0, 0});
-    CGE::Text::textRenderer::renderText(std::to_string(std::get<0>(gameInfo)), 0.73, -0.65f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText("LEVEL", INFO_X_POSITION, -0.5f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText(std::to_string(std::get<0>(gameInfo)), INFO_X_POSITION, -0.65f, 0.4f, {0, 0, 0});
 
     //Line
-    CGE::Text::textRenderer::renderText("LINES", 0.73, -0.8f, 0.4f, {0, 0, 0});
-    CGE::Text::textRenderer::renderText(std::to_string(std::get<2>(gameInfo)), 0.73, -0.95f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText("LINES", INFO_X_POSITION, -0.8f, 0.4f, {0, 0, 0});
+    CGE::Text::textRenderer::renderText(std::to_string(std::get<2>(gameInfo)), INFO_X_POSITION, -0.95f, 0.4f, {0, 0, 0});
 }
 
 void PlayingState::keyCallBack(GLFWwindow *window, int key, int action)
